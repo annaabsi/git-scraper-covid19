3dcb0f718ee96c2cd3ae24515d1666bea32447a1
Anna Absi <anna.absi@gmail.com>
2021-05-26 18:19:35 -0500

    notebooks, script and github actions scraper

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# git-scraper-covid19
+[![scraper-daily](https://github.com/annaabsi/git-scraper-covid19/actions/workflows/main.yml/badge.svg)](https://github.com/annaabsi/git-scraper-covid19/actions/workflows/main.yml)
+
+<!-- PROJECT HEADER -->
+<br />
+<p align="center">
+  <a href="#">
+    <img src="https://data.larepublica.pe/avance-vacunacion-covid-19-peru/logo.png" alt="Logo" width="30%" >
+  </a>
+
+  <h3 align="center">Casos confirmados y muertes por la Covid-19 en Perú</h3>
+
+</p>
+
+<hr>
+
+## Instalación 
+
+### Linux (Probado en Python 3.8.1+)
+
+Automatizado con [Github Actions](.github/workflows/main.yml)
+
+Instalación
+```
+# Instala requerimientos de Python
+pip3 install -r requirements.txt
+```
+
+Obtener resultados
+```
+# Casos positivos de Covid-19
+python3 scraper-positivos.py
+# Fallecidos por Covid-19
+python3 scraper-fallecidos.py
+# Finalmente realizar commit
+```
+
+
+## Descripción
+
+El programa consta de dos scripts separados: 
+- scraper-positivos: obtiene [datos de "Datos Abiertos Perú"](https://www.datosabiertos.gob.pe/dataset/casos-positivos-por-covid-19-ministerio-de-salud-minsa) sobre casos positivos de COVID-19 - MINSA.
+- scraper-fallecidos: obtiene [datos de "Datos Abiertos Perú"](https://www.datosabiertos.gob.pe/dataset/fallecidos-por-covid-19-ministerio-de-salud-minsa) sobre fallecidos por COVID-19 - MINSA.
+
+### *scraper-positivos.py*
+
+Genera distintas tablas de salida con sus respetivas columnas que se encuentran dentro de la carpeta [resultados/](resultados/)
+
+1. [positivos_diarios.csv](resultados/positivos_diarios.csv): FECHA_RESULTADO,METODODX
+2. [positivos_acumulados.csv](resultados/positivos_acumulados.csv): FECHA_RESULTADO,METODODX
+3. [positivos_por_departamentos.csv](resultados/positivos_por_departamentos.csv): DEPARTAMENTO,METODODX
+4. [positivos_por_edades.csv](resultados/positivos_por_edades.csv): GRUPO_ETARIO,POSITIVOS,POBLACION,PORCENTAJE
+5. [fecha_corte_positivos.json](resultados/fecha_corte_positivos.json): fecha_corte
+
+
+### *scraper-fallecidos.py*
+
+Genera distintas tablas de salida con sus respetivas columnas que se encuentran dentro de la carpeta [resultados/](resultados/)
+
+1. [fallecidos_diarios.csv](resultados/fallecidos_diarios.csv): FECHA_FALLECIMIENTO,SEXO
+2. [fallecidos_acumulados.csv](resultados/fallecidos_acumulados.csv): FECHA_FALLECIMIENTO,SEXO
+3. [fallecidos_por_departamentos.csv](resultados/fallecidos_por_departamentos.csv): DEPARTAMENTO,SEXO
+4. [fallecidos_por_edades.csv](resultados/fallecidos_por_edades.csv): GRUPO_ETARIO,FALLECIDOS,POBLACION,PORCENTAJE
+5. [fecha_corte_fallecidos.json](resultados/fecha_corte_fallecidos.json): fecha_corte
---
+++ scraper-fallecidos.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import requests
+from io import StringIO
+
+url = "https://cloud.minsa.gob.pe/s/Md37cjXmjT9qYSa/download"
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
+req = requests.get(url, headers=headers)
+data = StringIO(req.text)
+
+import pandas as pd
+df=pd.read_csv(data, sep=';', usecols=['FECHA_CORTE', 'EDAD_DECLARADA', 'SEXO', 'FECHA_FALLECIMIENTO', 'DEPARTAMENTO'], parse_dates=['FECHA_FALLECIMIENTO'])
+#df=pd.read_csv('fallecidos_covid.csv', sep=';', usecols=['FECHA_CORTE', 'EDAD_DECLARADA', 'SEXO', 'FECHA_FALLECIMIENTO', 'DEPARTAMENTO'], parse_dates=['FECHA_FALLECIMIENTO'])
+fecha_corte=df['FECHA_CORTE'].drop_duplicates().set_axis(['fecha_corte'])
+fecha_corte.to_json("../resultados/fecha_corte_fallecidos.json")
+
+# DIARIO FALLECIDOS
+df_fallecidos=df[['FECHA_FALLECIMIENTO','SEXO', 'EDAD_DECLARADA']].groupby(['FECHA_FALLECIMIENTO', 'SEXO']).count()
+df_fallecidos=df_fallecidos.reset_index()
+df_fallecidos=df_fallecidos.pivot(index='FECHA_FALLECIMIENTO', columns='SEXO', values='EDAD_DECLARADA')
+df_fallecidos=df_fallecidos.rename_axis(None, axis=1)
+df_fallecidos=df_fallecidos.fillna(0).astype('int')
+df_fallecidos
+
+# ACUMULADO FALLECIDOS
+df_fallecidos_cum=df_fallecidos.cumsum()
+df_fallecidos_cum
+
+# ACUMULADO POR DEPARTAMENTO
+df_fallecidos_departamento=df[['DEPARTAMENTO','SEXO','EDAD_DECLARADA']].groupby(['DEPARTAMENTO', 'SEXO']).count()
+df_fallecidos_departamento=df_fallecidos_departamento.reset_index()
+df_fallecidos_departamento=df_fallecidos_departamento.pivot(index='DEPARTAMENTO', columns='SEXO', values='EDAD_DECLARADA')
+df_fallecidos_departamento
+
+# ACUMULADO POR GRUPO ETARIO
+bins = [18,20,30,40,50,60,70,80,df['EDAD_DECLARADA'].max()+1]
+labels = ['18 a 19 años','20 a 29 años','30 a 39 años','40 a 49 años','50 a 59 años','60 a 69 años','70 a 79 años','80 años a más']
+poblacion_por_grupo_etario = [900000,5700000,5300000,4400000,3300000,2300000,1300000,700000] 
+df_fallecidos_edades = df
+df_fallecidos_edades['GRUPO_ETARIO'] = pd.cut(df['EDAD_DECLARADA'], bins=bins, labels=labels, right=False)
+df_fallecidos_edades = df_fallecidos_edades.groupby(['GRUPO_ETARIO'])["SEXO"].count().reset_index()
+df_fallecidos_edades.rename(columns = {'SEXO':'FALLECIDOS'}, inplace = True)
+df_fallecidos_edades['POBLACION']=poblacion_por_grupo_etario
+df_fallecidos_edades['PORCENTAJE']=round(df_fallecidos_edades['FALLECIDOS']/df_fallecidos_edades['POBLACION']*100,2)
+df_fallecidos_edades=df_fallecidos_edades.set_index('GRUPO_ETARIO')
+df_fallecidos_edades
+
+df_fallecidos.to_csv('resultados/fallecidos_diarios.csv')
+df_fallecidos_cum.to_csv('resultados/fallecidos_acumulados.csv')
+df_fallecidos_departamento.to_csv('resultados/fallecidos_por_departamentos.csv')
+df_fallecidos_edades.to_csv('resultados/fallecidos_por_edades.csv')
