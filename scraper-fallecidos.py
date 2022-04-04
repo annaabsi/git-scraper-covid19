25aa003e6114daf4d477154c4fc822ed190bc596
Anna Absi <anna.absi@gmail.com>
2022-04-04 00:30:28 -0500

    actualizacion de link

--- scraper-fallecidos.py
+++ scraper-fallecidos.py
@@ -14,7 +14,7 @@ def summary_by_department(df):
   return df
 
 try:
-  url = "https://cloud.minsa.gob.pe/s/xJ2LQ3QyRW38Pe5/download"
+  url = "https://files.minsa.gob.pe/s/t9AFqRbXw3F55Ho/download"
   headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
   req = requests.get(url, headers=headers)
   data = StringIO(req.text)
--- scraper-positivos.py
+++ scraper-positivos.py
@@ -15,7 +15,7 @@ def summary_by_department(df):
 
 
 try:
-  url = "https://cloud.minsa.gob.pe/s/AC2adyLkHCKjmfm/download"
+  url = "https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download"
   headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
   req = requests.get(url, headers=headers)
   data = StringIO(req.text)
